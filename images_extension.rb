4fdb08506e068664b221394adbd7743cf31e603b
Mario Visic <mario@squaretalent.com>
2010-08-28 00:40:10 +0800

    fixed a failing check for the settings extension

--- images_extension.rb
+++ images_extension.rb
@@ -20,7 +20,7 @@ class ImagesExtension < Radiant::Extension
   
   def activate
     
-    unless Radiant::Extension.descendants.any? { |extension| extension.class == 'SettingsExtension' }
+    unless Radiant::Extension.descendants.any? { |extension| extension.extension_name == 'Settings' }
       warn 'Error: The Images extension requires the Settings extension to be installed.'
       warn 'Either install the Settings extension or remove Images.'
       exit(1)
@@ -30,7 +30,6 @@ class ImagesExtension < Radiant::Extension
       Radiant::AdminUI.send :include, Images::AdminUI
       admin.image = Radiant::AdminUI.load_default_image_regions
     end
-    
     
     
     Page.send :include, Images::ImageTags, Images::PageExtensions
