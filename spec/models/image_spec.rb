b7e683218b3abad0464e98fbe97d00df0aaeef43
Mario Visic <mario@squaretalent.com>
2010-08-29 00:17:13 +0800

    improved specs for the image model

--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -12,6 +12,12 @@ describe Image do
     @image.title.should == 'first'
   end
   
+  it 'should require a unique title' do
+    @new_image = @image.clone
+    @new_image.valid?
+    @new_image.errors.on(:title).should include 'name is already in use'
+  end
+  
   context 's3 assets' do
     it 'should have an asset' do
       @image.asset.class.should == Paperclip::Attachment
@@ -29,9 +35,26 @@ describe Image do
       @image.asset_file_size.should == "1000"
     end
     
-    it 'should have filters' do
- 
+    it 'should require a valid asset' do
+      # create an invalid image without an asset!
+      image = Image.new
+      image.valid?
+      image.errors.on(:asset).should include 'must be set'
     end
+    
+    it 'should require a unique filename' do
+      @new_image = @image.clone
+      @new_image.valid?
+      @new_image.errors.on(:asset_file_name).should include 'file already exists'
+    end
+    
+    it 'should only allow image content types' do
+      @image.asset_content_type = 'not/image'
+      @image.valid?
+      @image.errors.on(:asset).should include 'not one of the allowed file types'
+    end
+    
+
   end
 
   
