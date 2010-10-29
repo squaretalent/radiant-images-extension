b40eb85134b7133a4a475b9a6466db93d5cc41a2
Mario Visic <mario@mariovisic.com>
2010-10-29 08:10:39 +0800

    Added specs for the method used to catch S3 exceptions in the admin images controller.

--- app/controllers/admin/images_controller.rb
+++ app/controllers/admin/images_controller.rb
@@ -1,15 +1,14 @@
 class Admin::ImagesController < Admin::ResourceController
   
-  before_filter :index_assets, :only => [ :index ]
-  before_filter :edit_assets, :only => [ :show, :edit ]
-  around_filter :rescue_s3_exceptions, :only => [:create, :edit, :destroy]
+  before_filter :index_assets,          :only => [ :index ]
+  before_filter :edit_assets,           :only => [ :show, :edit ]
+  around_filter :rescue_s3_exceptions,  :only => [ :create, :update, :destroy ]
   
   def index
     @images = Image.paginate :page => params[:page], :per_page => params[:pp] || 25
   end
   
-  
-private
+  protected
 
   def index_assets
     include_stylesheet 'admin/extensions/images/index'
--- spec/controllers/admin/images_controller_spec.rb
+++ spec/controllers/admin/images_controller_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe Admin::ImagesController do
+  
   dataset :users
   dataset :images
   
@@ -8,41 +9,79 @@ describe Admin::ImagesController do
     @image = images(:first)
     @images = Image.all
   end
-   
-  context 'user not logged in' do
-
-    it 'should redirect to login path' do
-      get :index
-      response.should redirect_to login_path
-    end
-
-  end
   
-  context 'user logged in' do
-    before :each do
-      login_as :admin
-      stub(AWS::S3::Base).establish_connection!
+  context 'index action' do
+    
+    context 'user not logged in' do
+
+      it 'should redirect to login path' do
+        get :index
+        response.should redirect_to login_path
+      end
+
     end
     
-    context 'index action' do
-    
+    context 'user logged in' do
+
+      before :each do
+        login_as :admin
+        stub(AWS::S3::Base).establish_connection!
+      end
+
       it 'should render the index template on index action' do
         get :index
         response.should render_template(:index)
       end
-      
+
       it 'should have a list of images' do
         get :index
         assigns(:images).should == @images
       end
-      
+
       it 'should use pagination' do
         get :index
         assigns(:images).class.should == WillPaginate::Collection
       end
+
+    end
     
-    end  
+    context 'rescusing S3 exceptions' do
+      
+      before :each do
+        controller = Admin::ImagesController.new
+      end
+      
+      it 'should catch any AWS S3 response errors thrown' do
+        stub(controller).redirect_to(anything)
+        controller.send :rescue_s3_exceptions do
+          raise AWS::S3::ResponseError.new('error name', '')
+        end
+      end
+      
+      it 'should set the flash message' do
+        stub(controller).redirect_to(anything)
+        controller.send :rescue_s3_exceptions do
+          raise AWS::S3::ResponseError.new('some error' ,'')
+        end
+        (controller.send :flash)[:error].should == 'some error'
         
-  end
+      end
+      
+      it 'should redirect to the admin images url' do
+        mock(controller).redirect_to(admin_images_url)
+        controller.send :rescue_s3_exceptions do
+          raise AWS::S3::ResponseError.new('error' ,'')
+        end
+      end
+      
+      it 'should not catch any other errors thrown' do
+        lambda { controller.send :rescue_s3_exceptions do
+          raise 'another type of error'
+        end }.should raise_error 'another type of error'
+      end
+      
+    end
     
+  end
+      
 end
