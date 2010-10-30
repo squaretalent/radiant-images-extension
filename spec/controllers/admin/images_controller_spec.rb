68cda2e39114c1cab6487beceb160dee563750b5
Mario Visic <mario@mariovisic.com>
2010-10-30 21:39:22 +0800

    Added some specs for the admin images controller.

--- spec/controllers/admin/images_controller_spec.rb
+++ spec/controllers/admin/images_controller_spec.rb
@@ -42,46 +42,87 @@ describe Admin::ImagesController do
         get :index
         assigns(:images).class.should == WillPaginate::Collection
       end
+      
+      it 'should include the index stylesheet' do
+        mock(controller).index_assets
+        get :index
+      end
 
     end
-    
-    context 'rescusing S3 exceptions' do
-      
-      before :each do
-        controller = Admin::ImagesController.new
-      end
-      
-      it 'should catch any AWS S3 response errors thrown' do
-        stub(controller).redirect_to(anything)
-        controller.send :rescue_s3_exceptions do
-          raise AWS::S3::ResponseError.new('error name', '')
-        end
-      end
-      
-      it 'should set the flash message' do
-        stub(controller).redirect_to(anything)
-        controller.send :rescue_s3_exceptions do
-          raise AWS::S3::ResponseError.new('some error' ,'')
-        end
-        (controller.send :flash)[:error].should == 'some error'
         
+  end
+    
+  context 'edit action' do
+  
+    context 'user logged in' do
+
+      before :each do
+        login_as :admin
+        stub(AWS::S3::Base).establish_connection!
       end
       
-      it 'should redirect to the admin images url' do
-        mock(controller).redirect_to(admin_images_url)
-        controller.send :rescue_s3_exceptions do
-          raise AWS::S3::ResponseError.new('error' ,'')
-        end
+      it 'should include the edit stylesheet' do
+        mock(controller).edit_assets
+        get :edit, :id => @image.id
+      end
+    
+    end  
+    
+  end
+  
+  context 'show action' do
+  
+    context 'user logged in' do
+
+      before :each do
+        login_as :admin
+        stub(AWS::S3::Base).establish_connection!
       end
       
-      it 'should not catch any other errors thrown' do
-        lambda { controller.send :rescue_s3_exceptions do
-          raise 'another type of error'
-        end }.should raise_error 'another type of error'
+      it 'should include the edit stylesheet' do
+        mock(controller).edit_assets
+        get :show, :id => @image.id
       end
+    
+    end  
+    
+  end
+  
+  context 'rescusing S3 exceptions' do
+    
+    before :each do
+      controller = Admin::ImagesController.new
+    end
+    
+    it 'should catch any AWS S3 response errors thrown' do
+      stub(controller).redirect_to(anything)
+      controller.send :rescue_s3_exceptions do
+        raise AWS::S3::ResponseError.new('error name', '')
+      end
+    end
+    
+    it 'should set the flash message' do
+      stub(controller).redirect_to(anything)
+      controller.send :rescue_s3_exceptions do
+        raise AWS::S3::ResponseError.new('some error' ,'')
+      end
+      (controller.send :flash)[:error].should == 'some error'
       
+    end
+    
+    it 'should redirect to the admin images url' do
+      mock(controller).redirect_to(admin_images_url)
+      controller.send :rescue_s3_exceptions do
+        raise AWS::S3::ResponseError.new('error' ,'')
+      end
+    end
+    
+    it 'should not catch any other errors thrown' do
+      lambda { controller.send :rescue_s3_exceptions do
+        raise 'another type of error'
+      end }.should raise_error 'another type of error'
     end
     
   end
-      
+    
 end
