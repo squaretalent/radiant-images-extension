5377c7812d42b5f374c63da3b3eff7003bedcf9c
Dirk Kelly <dk@dirkkelly.com>
2011-01-04 12:07:26 +0800

    added the attachments routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ ActionController::Routing::Routes.draw do |map|
   
   map.namespace :admin, :member => { :remove => :get } do |admin|
     admin.resources :images
+    admin.resources :attachments, :collection => { :sort => :put }
   end
   
 end
